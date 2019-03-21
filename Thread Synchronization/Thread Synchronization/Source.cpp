0fcd954ef2d57862b310f4650baae1062141b6d4
Mikita Zhuryk <mikita.zhuryk@gmail.com>
2019-03-21 23:46:56 +0300

    Added Lab3 (Thread synchronization)

---
+++ Thread Synchronization/Thread Synchronization/Source.cpp
@@ -0,0 +1,132 @@
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+CRITICAL_SECTION cs_console_input;
+CRITICAL_SECTION cs_console_output;
+CRITICAL_SECTION cs_array_access;
+CRITICAL_SECTION cs_sum_end;
+HANDLE start_sum_event;
+
+struct Data {
+
+	volatile uint32_t* arr;
+	size_t size;
+	uint16_t k;
+	uint64_t result;
+};
+
+bool checkPrime(uint32_t n) {
+	bool prime = true;
+	for (uint32_t i = 2; i <= ceil(sqrt(n)); ++i) {
+		if (n % i == 0) {
+			prime = false;
+		}
+	}
+	return prime;
+}
+
+void processArray(volatile uint32_t* arr, size_t size, uint16_t delay, uint16_t k) {
+	size_t count = 0;
+	for (size_t i = 0; i < size; ++i) {
+		for (size_t j = i; j < size; ++j) {
+			if (checkPrime(arr[j]) && !checkPrime(arr[i])) {
+				swap(arr[i], arr[j]);
+			}
+		}
+		Sleep(delay);
+		if (i == k) {
+			LeaveCriticalSection(&cs_array_access);
+		}
+	}
+}
+
+DWORD WINAPI work_thread(void* data) {
+	EnterCriticalSection(&cs_array_access);
+	uint16_t delay;
+	EnterCriticalSection(&cs_console_output);
+	cout << "Enter delay after each element in ms\n";
+	LeaveCriticalSection(&cs_console_output);
+	EnterCriticalSection(&cs_console_input);
+	cin >> delay;
+	LeaveCriticalSection(&cs_console_input);
+	Data* d = (Data*)data;
+	volatile uint32_t* arr = d->arr;
+	size_t size = d->size;
+	uint16_t k = d->k;
+	processArray(arr, size, delay, k);
+	return 0;
+}
+
+DWORD WINAPI sum_thread(void* data) {
+	WaitForSingleObject(start_sum_event, INFINITE);
+	EnterCriticalSection(&cs_array_access);
+	Data* d = (Data*)data;
+	d->result = 0;
+	for (size_t i = 0; i < d->k; ++i) {
+		d->result += d->arr[i];
+	}
+	LeaveCriticalSection(&cs_array_access);
+	return 0;
+}
+
+void printToConsole(uint32_t* arr, size_t size) {
+	cout << "Array size: " << size << endl << "Array elements:\n";
+	for (size_t i = 0; i < size; ++i) {
+		cout << arr[i] << ' ';
+	}
+	cout << endl;
+}
+
+int main() {
+	InitializeCriticalSection(&cs_console_input);
+	InitializeCriticalSection(&cs_console_output);
+	InitializeCriticalSection(&cs_array_access);
+	start_sum_event = CreateEvent(NULL, FALSE, TRUE, NULL);
+	ios_base::sync_with_stdio(false);
+	size_t n;
+	cout << "Enter array length\n";
+	cin >> n;
+	uint32_t* arr = new uint32_t[n];
+	cout << "Enter array elements\n";
+	for (size_t i = 0; i < n; ++i) {
+		cin >> arr[i];
+	}
+	printToConsole(arr, n);
+	Data data;
+	data.arr = arr;
+	data.size = n;
+	unsigned work_id;
+	HANDLE hWorkThread = CreateThread(NULL, 2 * 1048576, work_thread, &data, CREATE_SUSPENDED, (LPDWORD)&work_id);
+	int k;
+	cout << "Enter k\n";
+	cin >> k;
+	data.k = k;
+	ResumeThread(hWorkThread);
+	unsigned sum_id;
+	//WaitForSingleObject(hWorkThread, INFINITE);
+	HANDLE hSumThread = CreateThread(NULL, 2 * 1048576, sum_thread, &data, NULL, (LPDWORD)&sum_id);
+	EnterCriticalSection(&cs_array_access);
+	EnterCriticalSection(&cs_console_output);
+	cout << "Array after sorting k elements\n";
+	for (size_t i = 0; i < data.size; ++i) {
+		cout << data.arr[i] << ' ';
+	}
+	cout << endl;
+	LeaveCriticalSection(&cs_console_output);
+	ResetEvent(start_sum_event);
+	LeaveCriticalSection(&cs_array_access);
+	EnterCriticalSection(&cs_array_access);
+	cout << "Checked cs_array_access from main\n";
+	LeaveCriticalSection(&cs_array_access);
+	WaitForSingleObject(hSumThread, INFINITE);
+	EnterCriticalSection(&cs_console_output);
+	cout << "Sum of the first " << k << " prime numbers in the array: " << data.result << endl;
+	LeaveCriticalSection(&cs_console_output);
+	CloseHandle(hWorkThread);
+	CloseHandle(hSumThread);
+	CloseHandle(start_sum_event);
+	system("pause");
+	return 0;
+}
